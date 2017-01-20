ef4a228825277c7be3b54e3e41d7197fe003d1a5
Florian Bernard <florianxbernard@gmail.com>
2017-01-20 18:28:21 -0500

    Add date picker for bootstrap
    Start validation form

--- js/add.js
+++ js/add.js
@@ -11,6 +11,11 @@ $(document).ready(function() {
             x++; //text box increment
             $("#occupations").append('<tr id="a' + x +'"><td><label for="oname">Nom de l\'occupation:</label></br><input type="text" class="form-control" id="oname" name="oname[]"></td><td><label for="oproject">Nom du projet:</label></br><input type="text" class="form-control" id="oproject" name="oproject[]"></td><td><label for="osuper">Nom du superviseur:</label></br><input type="text" class="form-control" id="osuper" name="osuper[]"></td><td><label for="ostart">Date de debut:</label></br><input type="text" class="form-control" id="ostart" name="ostart[]"></td><td><label for="oend">Date de fin:</label></br><input type="text" class="form-control" id="oend" name="oend[]"></td><td><a href="#" class="glyphicon glyphicon-remove remove_field" id="' + x + '" style="align: center"></a></td></tr>'); //add input box
         }
+        $("#ostart, #oend").datepicker({
+            format: 'yyyy-mm-dd',
+            todayHighlight: true,
+            autoclose: true,
+        });
     });
 
     $("#occupations").on("click",".remove_field", function(e){ //user click on remove text
@@ -24,9 +29,16 @@ $(document).ready(function() {
             y++; //text box increment
             $("#groupes").append('<tr id="b' + y +'"><td><label for="gname">Nom du groupe:</label></br><input type="text" class="form-control" id="gname" name="gname[]"> </td><td><label for="gstart">Date de debut:</label></br><input type="text" class="form-control" id="gstart" name="gstart[]"></td><td><label for="gend">Date de fin:</label></br><input type="text" class="form-control" id="gend" name="gend[]"></td><td><a href="#" class="glyphicon glyphicon-remove remove_field_groupe" id="' + y + '" style="align: center"></a></td></tr>'); //add input box
         }
+        $("#gstart, #gend").datepicker({
+            format: 'yyyy-mm-dd',
+            todayHighlight: true,
+            autoclose: true,
+        });
     });
 
     $("#groupes").on("click",".remove_field_groupe", function(e){ //user click on remove text
         e.preventDefault(); $("#b" + $(this).attr("id")).remove(); y--;
     })
+
+
 });
--- js/formhelp.js
+++ js/formhelp.js
@@ -1,9 +1,26 @@
 $(document).ready(function(){
-  $('.date').datepicker({
-    format: 'yyyy/mm/dd',
-    container: container,
-    todayHighlight: true,
-    autoclose: true,
-  });
+      $("#dob, #ostart, #oend, #gstart, #gend").datepicker({
+          format: 'yyyy-mm-dd',
+          todayHighlight: true,
+          autoclose: true,
+      });
+      $("#submit").click(function(){
+        if($("#name").val() !='') {
+          if($("#sim").val() !='') {
+            if($("#email").val() !='') {
 
-});
+            }
+            else {
+              alert ('Entrer une adresse courriel');
+            }
+          }
+          else {
+            alert ('Les informations SIM doivent etre remplies');
+          }
+        }
+        else {
+          alert ('Le nom et le prenom doit etre remplie');
+        }
+        return false;
+      });
+  });
