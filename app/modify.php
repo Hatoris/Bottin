8cec49518cb158789a0ec093c16ed23fa2902871
Florian Bernard <florianxbernard@gmail.com>
2017-01-29 23:49:26 -0500

    last touch to modify function everithing works
    need to add multy supervisor 
    need to add autocomplete

--- app/modify.php
+++ app/modify.php
@@ -27,6 +27,7 @@ class modifyInfo {
 
       function __construct($x)
       {
+            //print_r($x);
       //genrales informations
 
       $this->name = trim($x['name']) ;
@@ -48,9 +49,9 @@ class modifyInfo {
       $this->oprojects[] = trim($oproject);
       }
 
-      foreach ($x['osuper'] as $osupervisor)
+      foreach ($x['osuper'] as $osuper)
       {
-      $this->osupervisors[] = trim($osupervisor);
+      $this->osupervisors[] = trim($osuper);
       }
 
       foreach ($x['ostart'] as $ostart)
@@ -85,128 +86,11 @@ class modifyInfo {
 
       }
 
-       function __destruct()
-      {
-      //destruction
-
-      $this->name ;
-      $this->sim ;
-      $this->email ;
-      $this->dob ;
-      $this->office ;
-      $this->phoneext ;
-      $this->onames ;
-      $this->oprojects;
-      $this->osupervisors;
-      $this->ostarts;
-      $this->oends;
-      $this->gnames;
-      $this->gstarts;
-      $this->gends;
-      }
 
 
 
 
-
-
-
-
-
-
-
-
-public function modifyPeople() {
-  error_reporting(0);
-          $z=0;
-          $u=0;
-              $info = getInfo::callPeopleXML();
-
-                foreach($info->person as $v) {
-                $aa = getInfo::Compare($v->name, $this->name);
-                $ab = getInfo::Compare($v->sim, $this->sim);
-                $ac = getInfo::Compare($v->email, $this->email);
-                $y = $aa + $ab + $ac;
-                 //     echo $u . " => " . $y . "</br>";
-
-                if ($y >= 1) {
-                    $people = $info->person[$u];
-                    $du = array (
-                    $people->name => $this->name,
-                    $people->sim => $this->sim,
-                    $people->email => $this->email,
-                    $people->dob => $this->dob,
-                    $people->office => $this->office,
-                    $people->phoneext => $this->phoneext,
-              );
-
-                    if (count($people->occupation) < count($this->onames)){
-                          $rxd = count($this->onames);
-                    }
-                    else {
-                          $rxd = count($people->occupation);
-                    };
-                    for($mu=0; $mu<count($rxd); ++$mu) {
-                          $occ = $people->occupation[$mu];
-                          $dv = array (
-                          $occ->name => $this->onames[$mu],
-                          $occ->project => $this->oprojects[$mu],
-                          $occ->supervisor => $this->osupervisors[$mu],
-                          $occ->start => $this->ostarts[$mu],
-                          $occ->end => $this->oends[$mu]
-                        );
-                    }
-
-                    if (count($people->group) < count($this->gnames)){
-                          $rxt = count($this->gnames);
-                    }
-                    else {
-                          $rxt = count($people->group);
-                    };
-                    for($mz=0; $mu<count($rxt); ++$mz) {
-                          $gro = $people->occupation[$mz];
-                          $dw = array (
-                          $gro->name => $this->gnames[$mz],
-                          $occ->start => $this->gstarts[$mz],
-                          $occ->end => $this->gends[$mz]
-                              );
-                   }
-                   print_r ($du);
-
-              //Now if difference between things add it to xml file or change it.
-              //$du ; $dv ; $dw
-              foreach ($du as $key => $value) {
-                    if ($key != $value) {
-                          //change node values
-                          $key = $value;
-                         ++$z;
-                    }
-              }
-               foreach ($dv as $key => $value) {
-                    if ($key != $value) {
-                                //change node values
-                         $key = $value;
-                         ++$z;
-                   }
-             }
-                foreach ($dw as $key => $value) {
-                      if ($key != $value) {
-                            //change node values
-                         $key = $value;
-                         ++$z;
-                      }
-                }
-              }
-              ++$u;
-        }
-
-        //return print_r($du);
-        //return $z;
-}
-
-
-
-      public function modifyPeople3($a) {
+      public function modifyPeople($a) {
         //error_reporting(0);
                 $z=0;
                 $u=0;
@@ -231,7 +115,8 @@ public function modifyPeople() {
                        //print_r($this->onames) ;
                       if ($y >= 1) {
                           $people = $xml->person[$u];
-                          print_r($people);
+                         //print_r($people);
+                         print_r($this->gends);
 
                           if ($people->name != $this->name) {
                                 $r1 = !empty($this->name) ? $this->name : " " ;
@@ -263,36 +148,47 @@ public function modifyPeople() {
                             $people->phoneext = $r6;
 
                           }
+
                           $rxc = max(count($this->onames), count($this->oprojects), count($this->osupervisors), count($this->ostarts), count($this->oends));
                           $rxd = count($people->occupation);
-                          print_r($rxc);
-                          for($mu=0; $mu<count($rxd); $mu++) {
+                          $rxd2 = $rxd -1;
+                         //print_r($rxd);
+                         $ocr = array();
+                          for($mu=0; $mu<=$rxd2; $mu++) {
                                 $occ = $people->occupation[$mu];
+                                $ocr[] = $occ;
 
+                                if (!isset($this->onames[$mu]) && !isset($this->oprojects[$mu]) && !isset($this->osupervisors[$mu]) && !isset($this->ostarts[$mu]) && !isset($this->oends[$mu])) {
+                                      unset($people->occupation[$mu]);
+                                }
+                                else {
                                 if ($occ->name != $this->onames[$mu]){
                                       $mane = !empty($this->onames[$mu]) ? $this->onames[$mu] : " " ;
-                                  $occ->name =  $mane;
+                                      $occ->name =  $mane;
                                   }
 
-                                elseif ($occ->project != $this->oprojects[$mu]) {
+                                if ($occ->project != $this->oprojects[$mu]) {
                                       $prorect = !empty($this->oprojects[$mu]) ? $this->oprojects[$mu] : " " ;
-                                  $occ->project = $prorect;
+                                      $occ->project = $prorect;
                                 }
-                                elseif ($occ->supervisor != $this->osupervisors[$mu]) {
+                                if ($occ->supervisor != $this->osupervisors[$mu]) {
                                       $pruv = !empty($this->osupervisors[$mu]) ? $this->osupervisors[$mu] : " " ;
-                                  $occ->supervisor = $pruv;
+                                      $occ->supervisor = $pruv;
                                 }
-                                elseif ($occ->start != $this->ostarts[$mu]) {
+                                if ($occ->start != $this->ostarts[$mu]) {
                                       $reat = !empty($this->ostarts[$mu]) ? $this->ostarts[$mu] : " " ;
-                                  $occ->start = $reat ;
+                                      $occ->start = $reat ;
                                 }
-                                elseif ($occ->end != $this->oends[$mu]) {
-                                      $rend = !empty($this->oends[$mu]) ? $this->oends[$mu] : " " ;
-                                  $occ->end = $rend;
+                                if ($occ->end != $this->oends[$mu]) {
+                                    $rend = !empty($this->oends[$mu]) ? $this->oends[$mu] : " " ;
+                                    $occ->end = $rend;
                                 }
+                              }
                           }
-                          $upp = abs($rxc-$rxd);
-                          for($la= $rxc - 1; $la > $upp ;$la--) {
+
+                          if ($rxc - $rxd > 0) {
+                          $upp1 = self::myNumber($rxc);
+                          for($la = $upp1; $la < $rxc ; $la++) {
                               $occupation = $people->addChild('occupation');
                               $named = !empty($this->onames[$la]) ? $this->onames[$la] : " ";
                               $occupation->addChild('name', $named);
@@ -305,30 +201,39 @@ public function modifyPeople() {
                               $endd = !empty($this->oends[$la]) ? $this->oends[$la] : " ";
                               $occupation->addChild('end', $endd );
                           }
+                    }
+
 
                           $rxe =  max(count($this->gnames), count($this->gstarts), count($this->gends));
                           $rxf = count($people->group);
+                          $rxf2 = $rxf -1;
 
-                          for($mz=0; $mz<count($rxf); $mz++) {
+                          for($mz=0; $mz<=$rxf2; $mz++) {
                                 $gro = $people->group[$mz];
+                                if (!isset($this->gnames[$mz]) && !isset($this->gstarts[$mz]) && !isset($this->gends[$mz])) {
+                                      unset($people->group[$mz]);
+                                }
+                                else {
 
                                 if ($gro->name != $this->gnames[$mz]) {
                                       $x1 = !empty($this->gnames[$mz]) ? $this->gnames[$mz] : " " ;
                                   $gro->name = $x1;
                                 }
-                                elseif ($gro->start != $this->gstarts[$mz]) {
+                                if ($gro->start != $this->gstarts[$mz]) {
                                       $x2 = !empty($this->gstarts[$mz]) ? $this->gstarts[$mz] : " " ;
                                     $gro->start = $x2;
                                   }
 
-                                elseif ($gro->end != $this->gends[$mz]) {
+                                if ($gro->end != $this->gends[$mz]) {
                                       $x3 = !empty($this->gends[$mz]) ? $this->gends[$mz] : " " ;
                                     $gro->end = $x3;
                                   }
+                            }
                           }
                           //print_r($this->gnames);
-                          $up = abs($rxe-$rxf);
-                            for($lb= $rxe - 1;$lb > $up ;$lb--) {
+                          if ($rxe-$rxf > 0) {
+                          $up = self::myNumber($rxe);
+                            for($lb = $up; $lb < $rxe ; $lb++) {
                                     $groupe = $people->addChild('group');
                                     $nameg = !empty($this->gnames[$lb]) ? $this->gnames[$lb] : " " ;
                                     //echo $nameg;
@@ -339,7 +244,9 @@ public function modifyPeople() {
                                     $groupe->addChild('end', $endg );
 
                                 }
+                          }
 
+                                //print_r($xml);
                                 $xml->saveXML($a);
 
 
@@ -352,13 +259,57 @@ public function modifyPeople() {
 
 addInfo::reformat($a);
 self::__destruct();
-echo $this->name;
+            }
+
+            function myNumber($a) {
+                  if ($a == 1) {
+                        return 1;
+                  }
+                  else {
+                        $a -= 1;
+                        return $a;
+                  }
+            }
+
+
+            function __destruct()
+            {
+            //destruction
+
+            unset($this->name) ;
+            unset($this->sim );
+            unset($this->email );
+            unset($this->dob) ;
+            unset($this->office) ;
+            unset($this->phoneext) ;
+            unset($this->oname) ;
+            unset($this->oprojects);
+            unset($this->osupervisors);
+            unset($this->ostarts);
+            unset($this->oends);
+            unset($this->gnames);
+            unset($this->gstarts);
+            unset($this->gends);
             }
 
 
 
 
-  }
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 
